188d19f51eb33eaf741909f187c2727f5cbadc0d
Dominik <69728785+Dome1803@users.noreply.github.com>
2020-12-05 15:42:15 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -6,7 +6,6 @@ This module provides helper methods to analyze sales
 import pandas as pd
 
 
-
 def read_csv(file_name):
     """
     This function reads a CSV file in German format.
@@ -18,6 +17,7 @@ def read_csv(file_name):
 
 def to_csv(file_name, data_frame):
     data_frame.to_csv(file_name, sep=",", index=False)
+
 
 def to_datetime(data_frame):
     pd.to_datetime(data_frame, errors="coerce")

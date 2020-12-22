37ee9f235d67b3070cd172c9e5080db02000f3c3
Dominik <69728785+Dome1803@users.noreply.github.com>
2020-12-22 09:23:36 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -14,10 +14,26 @@ def read_csv(file_name):
     """
     return pd.read_csv(file_name, sep=",")
 
+def read_german_csv(file_name):
+    """
+    This function reads a CSV file in German format.
+    :param file_name: The file name.
+    :return: A data frame with the contents.
+    """
+    return pd.read_csv(file_name, sep=";")
+
 
 def to_csv(file_name, data_frame):
     data_frame.to_csv(file_name, sep=",", index=False)
 
+def to_german_csv(file_name, data_frame):
+    data_frame.to_csv(file_name, sep=";", decimal=",", index=False)
 
 def to_datetime(data_frame):
     pd.to_datetime(data_frame, errors="coerce")
+
+def to_qm(value):
+    result = value*0.092903
+    return result
+
+
